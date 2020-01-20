b0c13bdde2b4901d361a35d36fce64e6db337c2c
Joe Bernardi <gloveboxjoe@gmail.com>
2020-01-20 12:29:27 -0500

    whoops

---
+++ server/README.md
@@ -0,0 +1,3 @@
+** Notes
+
+- The API response from Giphy is pretty complicated (my guess: needing to support legacy clients), so in the name of trying to get the best performance, I looked at the URLs of the gifs featured in the actual Giphy trending page (https://giphy.com/explore/trending) and tried to gather which types of gif are actually in use.
--- shared/untyped.d.ts
+++ shared/untyped.d.ts
@@ -1,25 +1,25 @@
 declare module "bent" {
-    const bent: any;
-    export = bent;
+	const bent: any;
+	export = bent;
 }
 
 declare module "webfontloader" {
-    const webfontloader: any;
-    export = webfontloader;
+	const webfontloader: any;
+	export = webfontloader;
 }
 
 declare namespace JSX {
-    interface IntrinsicElements {
-        [elemName: string]: any;
-    }
+	interface IntrinsicElements {
+		[elemName: string]: any;
+	}
 }
 
 declare module "*.svg" {
-    const content: any;
-    export default content;
+	const content: any;
+	export default content;
 }
 
 declare module "*.jpg" {
-    const content: any;
-    export default content;
+	const content: any;
+	export default content;
 }
